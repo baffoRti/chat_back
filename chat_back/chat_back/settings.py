6338cb83aa7ce65dde0fb6f884aaefa9a1a4c952
baffoRti <saha14373@gmail.com>
2021-05-14 22:39:23 +0300

    123

--- chat_back/chat_back/settings.py
+++ chat_back/chat_back/settings.py
@@ -74,13 +74,6 @@ WSGI_APPLICATION = 'chat_back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
